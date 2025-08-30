d1a1bf5723a4b9f634a3a492a4ae2a909cec88ea
igorhnq <igorhenrique102@gmail.com>
2025-08-30 17:45:45 -0300

    feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,20 @@
+import CoffeeDeliveryLogo from "../../assets/images/coffee-delivery-logo.svg"
+
+import { MapPinIcon, ShoppingCartIcon } from "@phosphor-icons/react"
+
 export function Header() {
     return (
-        <h1>Header</h1>
+        <header className="flex justify-between items-center mt-8">
+            <img src={CoffeeDeliveryLogo} />
+            <div className="flex items-center gap-3">
+                <div className="flex items-center gap-1 bg-purple-light p-2 rounded-md text-purple-dark h-10">
+                    <MapPinIcon size={22} color="var(--color-purple)" weight="fill" />
+                    Porto Alegre, RS
+                </div>
+                <div className="bg-yellow-light p-2 rounded-md h-10">
+                    <ShoppingCartIcon size={22} fill="var(--color-yellow-dark)" weight="fill"  />
+                </div>
+            </div>
+        </header>
     )
 }
