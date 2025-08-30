09831a4a051ee000e7ef43d93b0e24e3a1fea660
igorhnq <igorhenrique102@gmail.com>
2025-08-30 20:44:31 -0300

    feat: add home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { HeroSection } from "./components/HeroSection"
+
+export function Home() {
+    return (
+        <>
+            <HeroSection />
+            <section className="mt-20">
+                <h2 className="text-3xl font-baloo text-base-subtitle font-extrabold">Nossos cafés</h2>
+            </section>
+        </>
+    )
+}
