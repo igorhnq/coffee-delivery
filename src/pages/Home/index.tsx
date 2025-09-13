c0cd64b02897e8605c77556b21653aed145cdba3
Igor Henrique <igorhenrique102@gmail.com>
2025-09-12 21:23:17 -0300

    refactor: move card component to home folder

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { HeroSection } from "./components/HeroSection"
-import { Card } from "../../components/Card"
+import { Card } from "./components/Card"
 import { coffeesData } from "../../coffees"
 
 export function Home() {
